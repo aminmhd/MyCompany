47c8ef41b9bc0759b2e1bd31d027dbcee6dddda9
amin mohammad <amin.m.abarashi@gmail.com>
2020-07-20 16:17:26 +0430

    MyCompany

--- resources/views/home/Layout/home.blade.php
+++ resources/views/home/Layout/home.blade.php
@@ -20,34 +20,7 @@
 
     <!-- Custom Theme Style -->
     <link href="{{ asset('build/css/custom.min.css') }}" rel="stylesheet">
-    {{--/////////////////////--}}
-{{--    <!--Google Font link-->
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:400,600,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
 
-
-    <link rel="stylesheet" href="{{ asset('assets/css/slick.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/slick-theme.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/fonticons.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/bootsnav.css') }}">
-
-
-    <!--For Plugins external css-->
-    <!--<link rel="stylesheet" href="assets/css/plugins.css" />-->
-
-    <!--Theme custom css -->
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-    <!--<link rel="stylesheet" href="assets/css/colors/maron.css">-->
-
-    <!--Theme Responsive css-->
-    <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}" />
-
-    <script src="{{ asset('assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js') }}"></script>--}}
 </head>
 
 <body class="login">
@@ -92,7 +65,7 @@
 
 
 
-<script src="{{ asset('assets/js/vendor/jquery-1.11.2.min.js') }}"></script>
+
 <script src="{{ asset('vendors/jquery/dist/jquery.min.js') }}"></script>
 <script src="{{ asset('js/sweetalert.js') }}"></script>
 @include('panel.notification.notification')
